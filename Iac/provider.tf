77725628f4032718e976ed89b971687d66e4917a
Abunuman <abunumanpharm@gmail.com>
2024-07-14 16:49:33 +0100

    variables.tf added

--- Iac/provider.tf
+++ Iac/provider.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "value"
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+
 provider "aws" {
+  access_key = "my_access_key"
+  secret_key = "my_secret_key"
   region = "us-east-1"
 }
